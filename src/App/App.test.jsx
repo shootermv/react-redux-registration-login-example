4119c447eb570925275e61aab33ae6daba4392a8
Moshe Vilner <moshevilner@Moshes-MacBook-Pro.local>
2019-01-04 16:19:31 +0200

    added enzyme configuration

---
+++ src/App/App.test.jsx
@@ -0,0 +1,8 @@
+// test file
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+// import {App} from './App'
+// var wrapper = enzyme.shallow(<App />);
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
