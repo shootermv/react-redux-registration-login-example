708e6cf3600a4a92be3cda5d13da73531d5d7544
Moshe Vilner <moshevilner@moshes-mbp.mynet>
2019-01-11 09:32:43 +0200

    moving tests to __tests_ folder everywhere

--- src/AdminPage/DeveloperDropZone/__tests__/DeveloperDropZone.test.jsx
+++ src/AdminPage/DeveloperDropZone/__tests__/DeveloperDropZone.test.jsx
@@ -1,7 +1,7 @@
 // test file
 import { shallow, mount, render } from 'enzyme';
 import React from 'react';
-import {DeveloperDropZone}  from './DeveloperDropZone'
+import {DeveloperDropZone}  from '../DeveloperDropZone'
 
 test('Should render DeveloperDropZone component', () => {
     const testUser = {
